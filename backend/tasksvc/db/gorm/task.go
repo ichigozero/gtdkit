b57ef88ad3f9e5540d7e34fc148e56996bed6de5
Gary Sentosa <gary.sentosa@gmail.com>
2022-04-14 10:49:05 +0900

    fix: small fixes

--- backend/tasksvc/db/gorm/task.go
+++ backend/tasksvc/db/gorm/task.go
@@ -13,28 +13,28 @@ func NewTaskRepository(db *libgorm.DB) tasksvc.TaskRepository {
 	return &taskRepository{db}
 }
 
-func (t taskRepository) Create(title, description string, userID uint64) (tasksvc.Task, error) {
+func (t *taskRepository) Create(title, description string, userID uint64) (tasksvc.Task, error) {
 	task := tasksvc.Task{Title: title, Description: description, Done: false, UserID: userID}
 	result := t.db.Create(&task)
 
 	return task, result.Error
 }
 
-func (t taskRepository) FindAll(userID uint64) ([]tasksvc.Task, error) {
+func (t *taskRepository) FindAll(userID uint64) ([]tasksvc.Task, error) {
 	var tasks []tasksvc.Task
 	result := t.db.Where("user_id = ?", userID).Find(&tasks)
 
 	return tasks, result.Error
 }
 
-func (t taskRepository) Find(userID, taskID uint64) (tasksvc.Task, error) {
+func (t *taskRepository) Find(userID, taskID uint64) (tasksvc.Task, error) {
 	var task tasksvc.Task
 	result := t.db.Where("id = ? AND user_id = ?", taskID, userID).First(&task)
 
 	return task, result.Error
 }
 
-func (t taskRepository) Update(task tasksvc.Task) (tasksvc.Task, error) {
+func (t *taskRepository) Update(task tasksvc.Task) (tasksvc.Task, error) {
 	tk, err := t.Find(task.UserID, task.ID)
 	if err != nil {
 		return tasksvc.Task{}, err
@@ -48,13 +48,13 @@ func (t taskRepository) Update(task tasksvc.Task) (tasksvc.Task, error) {
 			"user_id":     task.UserID,
 		})
 	if result.Error != nil {
-		return tasksvc.Task{}, err
+		return tasksvc.Task{}, result.Error
 	}
 
-	return tk, err
+	return tk, nil
 }
 
-func (t taskRepository) Delete(userID, taskID uint64) (bool, error) {
+func (t *taskRepository) Delete(userID, taskID uint64) (bool, error) {
 	task := tasksvc.Task{ID: taskID}
 	result := t.db.Where("user_id", userID).Delete(&task)
 
